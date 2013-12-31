a951b07955f3c622d0a1d51e20c5228005833549
Patrick Kimber <code@pkimber.net>
2013-12-31 11:25:33 +0000

    revert method names (this is still slightly confusing)

--- moderate/models.py
+++ moderate/models.py
@@ -82,11 +82,11 @@ class ModerateModel(models.Model):
         self.user_moderated = user
         self.moderate_state = moderate_state
 
-    def _set_pending(self, user):
+    def set_pending(self, user):
         self._set_moderated(user, ModerateState.pending())
 
-    def _set_published(self, user):
+    def set_published(self, user):
         self._set_moderated(user, ModerateState.published())
 
-    def _set_removed(self, user):
+    def set_removed(self, user):
         self._set_moderated(user, ModerateState.removed())
